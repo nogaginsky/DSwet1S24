1a64d47da422e6473f8759d0a1dd49f29164941e
danielmk <danielkup3@gmail.com>
2024-07-09 12:52:26 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/DSwet1S24.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/d/daniels_programs/from_the_site/academics/cs/data_structures/spring 24 ds/מטלות/DSwet1S24/code/main24b1.cpp" "/mnt/d/daniels_programs/from_the_site/academics/cs/data_structures/spring 24 ds/מטלות/DSwet1S24/cmake-build-debug/CMakeFiles/DSwet1S24.dir/code/main24b1.cpp.o"
+  "/mnt/d/daniels_programs/from_the_site/academics/cs/data_structures/spring 24 ds/מטלות/DSwet1S24/code/pirates24b1.cpp" "/mnt/d/daniels_programs/from_the_site/academics/cs/data_structures/spring 24 ds/מטלות/DSwet1S24/cmake-build-debug/CMakeFiles/DSwet1S24.dir/code/pirates24b1.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../code"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
